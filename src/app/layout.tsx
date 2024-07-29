0b08ec8674f3d2859b50e91b41b2adff3b589ecd
ndrxwsh <andreyshihalev099@gmail.com>
2024-07-29 18:50:09 +0700

    feat: add ym

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { YMInitializer } from 'react-yandex-metrika'
 
 import { Navigation } from '~/components/Navigation'
 import { ThemeProvider } from '~/components/ThemeProvider'
@@ -27,6 +28,7 @@ export default function RootLayout({
           'width-full bg-white text-primary antialiased dark:bg-black'
         )}
       >
+        <YMInitializer accounts={[97941467]} />
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           <Navigation />
           <div className='mx-auto max-w-[700px] px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20'>
