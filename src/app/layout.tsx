a1178b12b62441c61ebefdaa7cc80a9b4e06e637
ndrxwsh <andreyshihalev099@gmail.com>
2024-08-12 15:50:07 +0700

    fix: ym

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import clsx from 'clsx'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
-import Script from 'next/script'
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 
 import { Navigation } from '~/components/Navigation'
 import { ThemeProvider } from '~/components/ThemeProvider'
 
 import './globals.css'
+import { Metrika } from './metrika'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,23 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <head>
-        <Script id='yandex-metrika'>
-          {`   
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(97941467, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-          `}
-        </Script>
-      </head>
       <body
         className={clsx(
           inter.className,
@@ -60,6 +43,9 @@ export default function RootLayout({
             alt=''
           />
         </div>
+        <Suspense>
+          <Metrika />
+        </Suspense>
       </body>
     </html>
   )
---
+++ src/app/metrika.js
@@ -0,0 +1,33 @@
+'use client'
+
+import { usePathname, useSearchParams } from 'next/navigation'
+import Script from 'next/script'
+import { useEffect } from 'react'
+
+export const Metrika = () => {
+  const pathName = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    ym(97941467, 'hit', window.location.href)
+  }, [pathName, searchParams])
+
+  return (
+    <Script id='yandex-metrika'>
+      {`
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(97941467, "init", {
+          defer: true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+        });    
+      `}
+    </Script>
+  )
+}
